33f2e882da3324ba33e5a66fcda117baed333c7a
andreea-belu25 <andreeabelu254@gmail.com>
2024-01-04 22:37:02 +0200

    Model

--- app/views.py
+++ app/views.py
@@ -14,20 +14,4 @@ options = [
 ]
 
 def index(response):
-    if response.method == "POST":
-        print(response.POST.get('Prompt', ''))
-        params = {}
-        params['prompt'] = response.POST.get('Prompt', '')
-        params['samples'] = 4
-        params['width'] = 832
-        params['height'] = 1216
-
-        image_paths = generate_image(**params)
-        print(image_paths)
-
-        return render(response, "app/index.html", {
-            'image_paths': image_paths,
-            'options': options,
-        })
-
-    return render(response, "app/index.html", {'options': options})
+	return render(response, "app/index.html", {})
