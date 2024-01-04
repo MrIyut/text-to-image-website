026523d01e6855aedb90d5005084b76b2b0bb095
MrIyut <77267521+MrIyut@users.noreply.github.com>
2024-01-04 23:26:13 +0200

    Delete app/templates/app/scrpt.js

--- app/templates/app/scrpt.js
+++
@@ -1,8 +0,0 @@
-function toggleOptions() {
-    var optionsContainer = document.getElementById("optionsContainer");
-    if (optionsContainer.style.display === "none") {
-        optionsContainer.style.display = "block";
-    } else {
-        optionsContainer.style.display = "none";
-    }
-}
