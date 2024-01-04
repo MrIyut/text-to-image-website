0563b43e8aefd7eec382d4c4791e40e320c63ea2
andreea-belu25 <andreeabelu254@gmail.com>
2024-01-04 22:37:02 +0200

    Model

--- app/views.py
+++ app/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-	return render(response, "app/index.html", {})
+	options = [
+		{'label': 'Gandalf', 'value': 'Gandalf', 'image_url': 'Gandalf.jpg'},
+		{'label': 'Barbie', 'value': 'Barbie', 'image_url': 'Barbie.jpeg'},
+		{'label': 'Dracula', 'value': 'Dracula', 'image_url': 'Dracula.webp'},
+		{'label': 'Sauron', 'value': 'Sauron', 'image_url': 'Sauron.jpg'},
+		{'label': 'Scooby Doo', 'value': 'Scooby Doo', 'image_url': 'ScoobyDoo.jpeg'},
+		{'label': 'Harry Potter', 'value': 'Harry Potter', 'image_url': 'harrypotter.0.jpg'},
+		{'label': 'Mickey Mouse', 'value': 'Mickey Mouse', 'image_url': 'Mickey.jpg'},
+		
+	]
+
+	return render(response, "app/index.html", {'options': options})
