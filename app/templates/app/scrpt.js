cfa4f1df6a0c1c31b4a7bf31d370ba72368493ba
alexandra.marinica <alexandra.marinica@stud.acs.upb.ro>
2024-01-04 22:48:45 +0200

    options

---
+++ app/templates/app/scrpt.js
@@ -0,0 +1,8 @@
+function toggleOptions() {
+    var optionsContainer = document.getElementById("optionsContainer");
+    if (optionsContainer.style.display === "none") {
+        optionsContainer.style.display = "block";
+    } else {
+        optionsContainer.style.display = "none";
+    }
+}
